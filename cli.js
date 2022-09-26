b2945e5ca076f952f5dc1ca9259e09f2f1e4d196
Simon Kirsten <simonkirsten24@gmail.com>
2022-09-26 11:15:08 +0100

    Handle error case

--- cli.js
+++ cli.js
@@ -3,9 +3,14 @@
 const { spawnSync } = require("child_process");
 const { loadEnvConfig } = require("@next/env");
 
-loadEnvConfig(process.cwd(), process.env.NODE_ENV !== 'production');
+loadEnvConfig(process.cwd(), process.env.NODE_ENV !== "production");
 
 const args = process.argv.slice(2);
 
 const result = spawnSync(args[0], args.slice(1), { stdio: "inherit" });
-process.exit(result.status);
+if (result.error) {
+  process.stdout.write(result.error.message + "\n");
+  process.exit(1);
+} else {
+  process.exit(result.status);
+}
